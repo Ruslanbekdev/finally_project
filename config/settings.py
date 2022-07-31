e511b1db0f4a95faa652d0e4212381f76f5f48e2
ruslanbekdev <ioservice200212@gamil.com>
2022-07-31 18:02:28 +0500

    'full'

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -91,7 +91,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('DBUSER'),
+        'USER': os.environ.get('USER'),
         'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': 'localhost',
         'PORT': 5432,
